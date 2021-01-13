fccb261fd4712d23b4eb1a86ab4c85ad62094e02
stephen-leisy <stleisy@gmail.com>
2021-01-13 15:08:10 -0800

    created didUserWin fucntion and tested.

--- get-random-throw.js
+++ get-random-throw.js
@@ -10,3 +10,22 @@ export function getRandomThrow(booger) {
         return 'scissors'
 
 }
+
+export function didUserWin(player, computer) {
+    if ((player === 'rock' && computer === 'paper') ||
+        (player === 'paper' && computer === 'scissors') ||
+        (player === 'scissors' && computer === 'rock')) {
+        return 'You Lose!'
+    }
+    if ((player === 'rock' && computer === 'rock') ||
+        (player === 'paper' && computer === 'paper') ||
+        (player === 'scissors' && computer === 'scissors')) {
+        return 'We Have A Draw'
+    }
+    if ((player === 'paper' && computer === 'rock') ||
+        (player === 'scissors' && computer === 'paper') ||
+        (player === 'rock' && computer === 'scissors')) {
+        return 'You Won!'
+    }
+
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,6 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { getRandomThrow } from '../get-random-throw.js';
+import { getRandomThrow, didUserWin } from '../get-random-throw.js';
 
 
 const test = QUnit.test;
@@ -31,14 +31,39 @@ test('it should return paper when presented with 2', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
-test('it should return scissors when presented with 3', (expect) => {
+test('it should return lose when you enter rock and paper', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = 'scissors';
+    const expected = 'You Lose!';
 
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = getRandomThrow(3);
+    const actual = didUserWin('rock', 'paper');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('it should return draw when you enter rock and rock', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'We Have A Draw';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = didUserWin('rock', 'rock');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+}); test('it should return win when you enter rock and scissors', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'You Won!';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = didUserWin('rock', 'scissors');
 
     //Expect
     // Make assertions about what is expected versus the actual result
